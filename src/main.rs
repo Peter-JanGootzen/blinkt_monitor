02a4c3030e47ed7335c651922d3b8155db0def0e
Peter-Jan Gootzen <peterjan@gootzen.net>
2020-02-16 20:03:03 +0100

    Added unix term signal handler

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,19 @@ mod tasks;
 use std::thread;
 use std::time::Duration;
 use std::vec::Vec;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc
+};
 use syslog::{Facility, Formatter3164, BasicLogger};
 use log::LevelFilter;
 use blinkt::Blinkt;
+use simple_signal::{self, Signal};
 
 use crate::tasks::Task;
 use crate::tasks::OnlineTask;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let formatter = Formatter3164 {
         facility: Facility::LOG_USER,
         hostname: None,
@@ -39,8 +44,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     blinkt.set_pixel_rgbb(7, 0, 10, 0, brightness);
     blinkt.show()?;
 
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    simple_signal::set_handler(&[Signal::Int, Signal::Term], move |_signals| {
+        r.store(false, Ordering::SeqCst);
+    });
+
     let interval = 100;
-    loop {
+    while running.load(Ordering::SeqCst) {
         for (task_index, (task, task_interval)) in (&tasks).into_iter().enumerate() {
             if tasks_cooldown[task_index] <= 0 {
                 task(&mut blinkt, brightness);
